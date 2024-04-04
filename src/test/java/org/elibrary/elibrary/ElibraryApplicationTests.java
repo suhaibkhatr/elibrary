2eab4dbfbc869be7397bfe888a0c4b7177cf9f3e
Suhaib Khater <s.khater@classera.com>
2024-04-04 14:15:15 -0500

    init commit

---
+++ src/test/java/org/elibrary/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elibrary.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
